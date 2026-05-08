b087703ebd946562c6d6b4695f700532896b19d1
Aleksandr <arokai1233@mail.ru>
2026-05-08 12:34:18 +0300

    Updated filter logic, will only view data that actually exists/connected with vehicles/photos. Added more default data.

--- backend/prisma/data/cities.ts
+++ backend/prisma/data/cities.ts
@@ -1,12 +1,69 @@
 export const cities = [
   { name: "Tallinn", countyName: "Harjumaa" },
-  { name: "Kohtla-Järve", countyName: "Ida-Virumaa" },
-  { name: "Tartu", countyName: "Tartumaa" },
+  { name: "Maardu", countyName: "Harjumaa" },
+  { name: "Keila", countyName: "Harjumaa" },
+  { name: "Saue", countyName: "Harjumaa" },
+  { name: "Paldiski", countyName: "Harjumaa" },
+  { name: "Kehra", countyName: "Harjumaa" },
+  { name: "Loksa", countyName: "Harjumaa" },
+
+  { name: "Kärdla", countyName: "Hiiumaa" },
+
   { name: "Narva", countyName: "Ida-Virumaa" },
+  { name: "Kohtla-Järve", countyName: "Ida-Virumaa" },
+  { name: "Sillamäe", countyName: "Ida-Virumaa" },
+  { name: "Jõhvi", countyName: "Ida-Virumaa" },
+  { name: "Kiviõli", countyName: "Ida-Virumaa" },
+  { name: "Narva-Jõesuu", countyName: "Ida-Virumaa" },
+  { name: "Püssi", countyName: "Ida-Virumaa" },
   { name: "Vaivara", countyName: "Ida-Virumaa" },
   { name: "Kudruküla", countyName: "Ida-Virumaa" },
   { name: "Linna", countyName: "Ida-Virumaa" },
   { name: "Soldina", countyName: "Ida-Virumaa" },
   { name: "Hiiemetsa", countyName: "Ida-Virumaa" },
   { name: "Vodava", countyName: "Ida-Virumaa" },
+
+  { name: "Jõgeva", countyName: "Jõgevamaa" },
+  { name: "Põltsamaa", countyName: "Jõgevamaa" },
+  { name: "Mustvee", countyName: "Jõgevamaa" },
+
+  { name: "Paide", countyName: "Järvamaa" },
+  { name: "Türi", countyName: "Järvamaa" },
+
+  { name: "Haapsalu", countyName: "Läänemaa" },
+  { name: "Lihula", countyName: "Läänemaa" },
+
+  { name: "Rakvere", countyName: "Lääne-Virumaa" },
+  { name: "Tapa", countyName: "Lääne-Virumaa" },
+  { name: "Kunda", countyName: "Lääne-Virumaa" },
+  { name: "Tamsalu", countyName: "Lääne-Virumaa" },
+
+  { name: "Põlva", countyName: "Põlvamaa" },
+  { name: "Räpina", countyName: "Põlvamaa" },
+
+  { name: "Pärnu", countyName: "Pärnumaa" },
+  { name: "Sindi", countyName: "Pärnumaa" },
+  { name: "Kilingi-Nõmme", countyName: "Pärnumaa" },
+
+  { name: "Rapla", countyName: "Raplamaa" },
+
+  { name: "Kuressaare", countyName: "Saaremaa" },
+
+  { name: "Tartu", countyName: "Tartumaa" },
+  { name: "Elva", countyName: "Tartumaa" },
+  { name: "Kallaste", countyName: "Tartumaa" },
+
+  { name: "Valga", countyName: "Valgamaa" },
+  { name: "Tõrva", countyName: "Valgamaa" },
+  { name: "Otepää", countyName: "Valgamaa" },
+
+  { name: "Viljandi", countyName: "Viljandimaa" },
+  { name: "Võhma", countyName: "Viljandimaa" },
+  { name: "Karksi-Nuia", countyName: "Viljandimaa" },
+  { name: "Mõisaküla", countyName: "Viljandimaa" },
+  { name: "Suure-Jaani", countyName: "Viljandimaa" },
+  { name: "Abja-Paluoja", countyName: "Viljandimaa" },
+
+  { name: "Võru", countyName: "Võrumaa" },
+  { name: "Antsla", countyName: "Võrumaa" },
 ];
